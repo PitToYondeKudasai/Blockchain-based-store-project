de32e35c10aee0ccb7e1bc3a8bdf62467e0d20ac
PitToYondeKudasai <buttaronipeter@gmail.com>
2020-12-19 21:42:39 +0100

    Create Ownable.sol

---
+++ final project/source code/Ownable.sol
@@ -0,0 +1,20 @@
+
+pragma solidity ^0.6.0;
+
+contract Ownable {
+    
+address public _owner;
+    constructor () internal { 
+        _owner = msg.sender;
+    }
+    
+    modifier onlyOwner() {
+        require(isOwner(), "You are not the owner"); 
+        _;
+    }
+    
+    function isOwner() public view returns (bool) { 
+        return (msg.sender == _owner);
+    } 
+    
+}
